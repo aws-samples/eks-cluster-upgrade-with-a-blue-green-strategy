f30e927a9ce802de239c4bc609fdff7910a58c5f
ShinChul Bang <shinchul@amazon.com>
2023-09-21 19:08:00 +0900

    Modify the code to return the default 0.0.0.0/0 when an error occurs in the findMyExternalIp function

--- aws-cdks/my-eks-blueprints/lib/utils/get-external-ip-address.ts
+++ aws-cdks/my-eks-blueprints/lib/utils/get-external-ip-address.ts
@@ -3,9 +3,9 @@ import axios from "axios"
 /**
  * Finds and returns my external IP address. 
  * If an error occurs while looking for an external IP address, 
- * the IP address of the local host is returned instead.
+ * the IP address of the 0.0.0.0/0 is returned instead.
  * 
- * @returns my external ip or 127.0.0.1
+ * @returns my external ip or 0.0.0.0
  */
 export async function findMyExternalIp(): Promise<string> {
   const url = "https://ipecho.io/json";
@@ -15,6 +15,6 @@ export async function findMyExternalIp(): Promise<string> {
     return response.data.ip+"/32"
   } catch(exception) {
     process.stderr.write(`ERROR received from ${url}: ${exception}\n`);
-    return "127.0.0.1/32";
+    return "0.0.0.0/0";
   }
 }
